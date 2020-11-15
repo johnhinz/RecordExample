c92e44d66e062f96a44ee15c8876a14459869d90
John Hinz <jhinz402@hotmail.com>
2020-11-14 22:44:35 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RecordTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                fName = "Joe", 
+                lName = "Smith", 
+                dob = DateTime.Now - new TimeSpan(30,0,0)
+            };
+        }
+    }
+
+    public record Person 
+    {
+        public string fName;
+        public string lName;
+        public DateTime dob;
+    };
+
+}
